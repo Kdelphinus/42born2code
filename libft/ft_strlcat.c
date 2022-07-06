add82b97a60aea430a97e95aa4875ab5faed31ed
Myoung jun Ko <myko@c3r3s2.42seoul.kr>
2022-07-06 20:09:37 +0900

    solve

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: myko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 19:39:01 by myko              #+#    #+#             */
-/*   Updated: 2022/07/06 20:01:46 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/06 20:09:18 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	return (dst);
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+
+	i = 0;
+	dst_len = 0;
+	src_len = 0;
+	while (dst[dst_len])
+		dst_len++;
+	while (src[src_len])
+		src_len++;
+	if (dstsize < dst_len + 1)
+		return (src_len + dstsize);
+	while (i < dstsize - 1 - dst_len && src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = 0;
+	return (dst_len + src_len);
 }
