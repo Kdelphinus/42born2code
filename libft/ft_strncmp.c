10f2c519071de06548b677408850523d3a019d5e
Myoung jun Ko <myko@c3r3s2.42seoul.kr>
2022-07-06 20:19:07 +0900

    solve

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: myko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/06 15:43:42 by myko              #+#    #+#             */
-/*   Updated: 2022/07/06 20:04:48 by myko             ###   ########.fr       */
+/*   Created: 2022/07/06 20:09:55 by myko              #+#    #+#             */
+/*   Updated: 2022/07/06 20:18:04 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i])
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	return (i);
+		if (!s1[i])
+			return (0);
+	}
+	return (0);
 }
