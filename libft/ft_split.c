33712d09b0acf3b097a4ed9b8e67f01611d2fa67
Myoung jun Ko <myko@c4r8s7.42seoul.kr>
2022-07-07 17:56:20 +0900

    crash

---
+++ libft/ft_split.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myko <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 17:04:03 by myko              #+#    #+#             */
+/*   Updated: 2022/07/07 17:56:12 by myko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	word_count(char const *s, char c)
+{
+	int	i;
+	int	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (s[i] == c)
+		i++;
+	while (s[i])
+	{
+		while (s[i] != c)
+			i++;
+		cnt++;
+		while (s[i] == c)
+			i++;
+	}
+	return (cnt);
+}
+
+char	**ft_str_input(char const *s, char **ss, char c)
+{
+	int	i;
+	int	e;
+	int	k;
+	int	j;
+
+	i = 0;
+	k = 0;
+	j = 0;
+	while (s[i] == c)
+		i++;
+	e = i;
+	while (s[i])
+	{
+		while (s[e] != c)
+			e++;
+		ss[k] = (char *)malloc(sizeof(char) * (e - i + 2));
+		if (ss[k] == 0)
+			return (0);
+		while (i < e)
+			ss[k][j++] = s[i++];
+		ss[k++][j] = 0;
+		j = 0;
+		while (s[i] == c)
+			i++;
+		e = i;
+	}
+	ss[k] = 0;
+	return (ss);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		w_cnt;
+	char	**ss;
+
+	w_cnt = word_count(s, c);
+	if (w_cnt == 0)
+		return (0);
+	ss = (char **)malloc(sizeof(char *) * (w_cnt + 1));
+	if (ss == 0)
+		return (0);
+	return (ft_str_input(s, ss, c));
+}
