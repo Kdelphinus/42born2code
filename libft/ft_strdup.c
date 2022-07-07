def662c97d6e3e67c557bff345a2a0f364ebd1a4
Myoung jun Ko <myko@c4r8s7.42seoul.kr>
2022-07-07 15:40:57 +0900

    solve

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: myko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:28:16 by myko              #+#    #+#             */
-/*   Updated: 2022/07/07 15:29:12 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/07 15:40:19 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 char	*ft_strdup(const char *s1)
 {
+	int		i;
+	int		s1_len;
+	char	*s2;
 
+	i = 0;
+	s1_len = ft_strlen(s1);
+	s2 = (char *)malloc(sizeof(char) * (s1_len + 1));
+	if (s2 == 0)
+		return (0);
+	while (i < s1_len)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = 0;
+	return (s2);
 }
