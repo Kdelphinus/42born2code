4d6c78a305e4d5a3c03c81f85a75f4916536e2fb
Myoung jun Ko <myko@c2r3s5.42seoul.kr>
2022-07-08 07:30:38 +0900

    modify

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 19:19:19 by myko              #+#    #+#             */
-/*   Updated: 2022/07/08 05:38:36 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/08 07:20:05 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	i;
-	unsigned int	s_len;
-	char			*cp;
+	size_t	i;
+	size_t	s_len;
+	size_t	sstart;
+	char	*cp;
 
 	i = 0;
+	sstart = (size_t)start;
 	s_len = ft_strlen(s);
-	if (start >= s_len)
+	if (sstart >= s_len)
 		return (ft_strdup(""));
-	cp = (char *)malloc(sizeof(char) * (s_len - start) + 1);
+	if (s_len - sstart < len)
+		cp = (char *)malloc(sizeof(char) * (s_len - sstart) + 1);
+	else
+		cp = (char *)malloc(sizeof(char) * len + 1);
 	if (cp == 0)
 		return (0);
-	while (i < len && s[i + start])
+	while (i < len && s[i + sstart])
 	{
-		cp[i] = s[i + start];
+		cp[i] = s[i + sstart];
 		i++;
 	}
 	cp[i] = 0;
