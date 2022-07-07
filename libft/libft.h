f866b63f15901f168eedd8ad549d668e39d86c5c
Myoung jun Ko <myko@c4r8s7.42seoul.kr>
2022-07-07 17:02:41 +0900

    add ft_calloc

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:07:51 by myko              #+#    #+#             */
-/*   Updated: 2022/07/07 16:25:22 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/07 16:58:36 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
