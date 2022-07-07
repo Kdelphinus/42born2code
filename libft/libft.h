6f7883d8f3a97f639a57119e8678f845e66c07ad
Myoung jun Ko <myko@c4r8s7.42seoul.kr>
2022-07-07 16:25:51 +0900

    norm

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 16:07:51 by myko              #+#    #+#             */
-/*   Updated: 2022/07/07 16:11:21 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/07 16:25:22 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
