786f4e0e72fcc149e7fcb4d13501153f47c785c5
Myoung jun Ko <myko@c3r6s1.42seoul.kr>
2022-07-13 18:26:22 +0900

    solving

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:42:10 by myko              #+#    #+#             */
-/*   Updated: 2022/07/13 15:55:57 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/13 18:03:43 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,30 @@
 
 char	*get_next_line(int fd)
 {
+	int		i;
+	int		j;
+	ssize_t	nr;
+	char	*buff;
+	char	*cbuff;
 
+	buff = (char *)malloc(BUFFER_SIZE);
+	if (buff == 0)
+		return (0);
+	nr = read(fd, buff, BUFFER_SIZE);
+	while (nr > 0)
+	{
+		i = 0;
+		while (i < nr && buff[i] != '\n')
+			i++;
+		j = 0;
+		cbuff = (char *)malloc(i);
+		while (j < i)
+		{
+			cbuff[j] = buff[j];
+			j++;
+		}
+		free(buff);
+		return (cbuff);
+	}
+	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:43:07 by myko              #+#    #+#             */
-/*   Updated: 2022/07/13 15:45:22 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/13 17:03:55 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+char	*get_next_line(int fd);
 
 #endif
