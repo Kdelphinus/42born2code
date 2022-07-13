72b0351f1e365a02395e462b3a7194155672ff4a
Myoung jun Ko <myko@c3r6s1.42seoul.kr>
2022-07-13 16:21:38 +0900

    modified

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,11 +6,29 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 19:17:18 by myko              #+#    #+#             */
-/*   Updated: 2022/07/12 19:35:13 by myko             ###   ########.fr       */
+/*   Updated: 2022/07/13 16:07:50 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	overflow_up_check(long long result, char num)
+{	
+	if (result == 922337203685477580 && num > '7')
+		return (0);
+	if (result > 922337203685477580)
+		return (0);
+	return (1);
+}
+
+static int	overflow_down_check(long long result, char num)
+{
+	if (result == -922337203685477580 && num > '8')
+		return (0);
+	if (result < -922337203685477580)
+		return (0);
+	return (1);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -31,9 +49,9 @@ int	ft_atoi(const char *str)
 	}
 	while (ft_isdigit(str[i]))
 	{
-		if (result >= 922337203685477580 && str[i] > '7')
+		if (!overflow_up_check(result, str[i]))
 			return (-1);
-		if (result <= -922337203685477580 && str[i] > '8')
+		if (!overflow_down_check(result, str[i]))
 			return (0);
 		result = result * 10 + (str[i] - '0') * sign;
 		i++;
