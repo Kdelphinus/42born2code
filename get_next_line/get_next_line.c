6279fa00ddc10072370a7803b7cdab07192069d1
Kdelphinus <kmj951015@gmail.com>
2022-08-03 18:55:10 +0900

    update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -37,7 +37,7 @@ char	*ft_read(int fd, char **line)
 	}
 	if (idx >= 0)
 	{
-		ft_strlcpy(tmp, buff, idx + 2);
+		ft_strlcpy(tmp, buff, idx + 1);
 		c_line = ft_strjoin(c_line, tmp);
 		*line = ft_strdup(buff + idx + 1);
 	}
--- get_next_line/simple_test.sh
+++ get_next_line/simple_test.sh
@@ -5,5 +5,9 @@ read b_size
 
 gcc -Wall -Wextra -Werror main.c get_next_line.c get_next_line_utils.c -I. get_next_line.h -D BUFFER_SIZE=$b_size
 
-echo "\nresult: "
+echo "\nsolution: "
+cat test.txt
+
+echo "result: "
 ./a.out
+
