5b02f2ea3489a143318fd1579367ceac47e29309
Kdelphinus <kmj951015@gmail.com>
2022-09-21 16:14:59 +0900

    studying...

---
+++ fract-ol/fractol.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 16:07:38 by myko              #+#    #+#             */
+/*   Updated: 2022/09/21 16:10:53 by myko             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "mlx/mlx.h"
+# include <stdlib.h>
+
+# define X_EVENT_KEY_PRESS	2
+# define X_EVENT_KEY_EXIT	17
+# define KEY_ESC			53
+
+typedef struct s_complex
+{
+	int	real;
+	int	imaginary;
+}			t_complex;
+
+typedef struct s_img
+{
+	void	*img_ptr;
+	int		*data;
+	int		size_l;
+	int		bpp;
+	int		endian;
+}			t_img;
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win;
+}			t_mlx;
+
+#endif
