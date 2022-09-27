a16282bb8169f05247248df09ffe2ff1275e79e9
Kdelphinus <kmj951015@gmail.com>
2022-09-27 19:52:51 +0900

    modified error message

--- fract-ol/srcs/utils_2.c
+++ fract-ol/srcs/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 17:17:48 by myko              #+#    #+#             */
-/*   Updated: 2022/09/27 17:35:17 by myko             ###   ########.fr       */
+/*   Updated: 2022/09/27 19:52:39 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_error(void)
 {
 	printf("❌ Please enter one of the parameters below. ❌\n");
 	printf("- mandelbrot\n");
-	printf("- julia\n");
 	printf("- julia [c_real] [c_imagin]\n");
+	printf("- multibrot [d]\n");
 	return (-1);
 }
