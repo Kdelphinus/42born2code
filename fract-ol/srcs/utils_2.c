5ffd040246c2e50f2f50f819e7dabe4c70d853be
Kdelphinus <kmj951015@gmail.com>
2022-09-30 20:08:44 +0900

    add error message

--- fract-ol/srcs/utils_2.c
+++ fract-ol/srcs/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 17:17:48 by myko              #+#    #+#             */
-/*   Updated: 2022/09/27 22:07:40 by myko             ###   ########.fr       */
+/*   Updated: 2022/09/30 20:08:12 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	ft_error(void)
 {
 	printf("❌ Please enter one of the parameters below. ❌\n");
 	printf("- mandelbrot\n");
-	printf("- julia [c_real] [c_imagin]\n");
+	printf("- julia [c_r] [c_i]\n");
 	printf("- multibrot [d]\n");
+	printf("- mandelbox [scale]")
 	return (-1);
 }
