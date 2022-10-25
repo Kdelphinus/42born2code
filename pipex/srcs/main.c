cd56db6696ef3fb12dd33d58262c00790bd9b4da
Kdelphinus <kmj951015@gmail.com>
2022-10-25 20:13:10 +0900

    update

--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:45:17 by myko              #+#    #+#             */
-/*   Updated: 2022/10/25 19:12:52 by myko             ###   ########.fr       */
+/*   Updated: 2022/10/25 20:12:27 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,15 +49,15 @@ char	*ft_path(char *filename, char **envp)
 	return (NULL);
 }
 
-void	child_pid(int fds[], char **argv, char **envp)
+void	child_pid(int fds2[], char **argv, char **envp)
 {
 	int		i;
 	char	*path;
 	char	**new_argv;
 
 	i = -1;
-	dup2(fds[1], STDOUT_FILENO);
-	close(fds[0]);
+	dup2(fds2[1], STDOUT_FILENO);
+	close(fds2[0]);
 	new_argv = ft_split(ft_strjoin(ft_strjoin(argv[2], " "), argv[1]), ' ');
 	path = ft_path(new_argv[0], envp);
 	if (!path)
@@ -69,22 +69,23 @@ void	child_pid(int fds[], char **argv, char **envp)
 	if (execve(path, new_argv, envp) == -1)
 	{
 		ft_printf("run error: %s\n", strerror(errno));
+		all_free(&new_argv);
 		exit(EXIT_FAILURE);
 	}
 }
 
-void	parent_pid(int fds[], char **argv, char **envp)
+void	parent_pid(int fds2[], char **argv, char **envp)
 {
 	int		i;
-	int		*a;
+	int		*b;
 	char	*path;
 	char	**new_argv;
 
 	i = -1;
-	a = (int *)malloc(sizeof(a));
-	waitpid(0, a, 0);
-	dup2(fds[0], STDIN_FILENO);
-	close(fds[1]);
+	b = (int *)malloc(sizeof(int));
+	waitpid(0, b, 0);
+	dup2(fds2[0], STDIN_FILENO);
+	close(fds2[1]);
 	new_argv = ft_split(argv[3], ' ');
 	path = ft_path(new_argv[0], envp);
 	if (!path)
@@ -96,6 +97,7 @@ void	parent_pid(int fds[], char **argv, char **envp)
 	if (execve(path, new_argv, envp) == -1)
 	{
 		ft_printf("run error: %s\n", strerror(errno));
+		all_free(&new_argv);
 		exit(EXIT_FAILURE);
 	}
 }
@@ -103,7 +105,10 @@ void	parent_pid(int fds[], char **argv, char **envp)
 int	main(int argc, char **argv, char **envp)
 {
 	int		fds[2];
+	int		fds2[2];
+	int		*a;
 	pid_t	pid;
+	pid_t	pid2;
 
 	if (argc != 5)
 	{
@@ -122,9 +127,40 @@ int	main(int argc, char **argv, char **envp)
 		exit(EXIT_FAILURE);
 	}
 	if (pid == 0)
-		child_pid(fds, argv, envp);
+	{
+		if (pipe(fds2) == -1)
+		{
+			ft_printf("pipe error: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		pid2 = fork();
+		if (pid2 == -1)
+		{
+			ft_printf("fork error: %s\n", strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		if (pid2 == 0)
+			child_pid(fds2, argv, envp);
+		else
+			parent_pid(fds2, argv, envp);
+		int	rd;
+		char *buff;
+		buff = malloc(1000001);
+		rd = read(fds2[0], buff, 1000000);
+		buff[rd] = 0;
+		printf("read: %s\n read end\n", buff);
+	}
 	else
-		parent_pid(fds, argv, envp);
-	printf("end\n");
+	{
+		a = (int *)malloc(sizeof(int));
+		waitpid(0, a, 0);
+		// printf("end\n");
+		// int	rd;
+		// char *buff;
+		// buff = malloc(1000001);
+		// rd = read(0, buff, 1000000);
+		// buff[rd] = 0;
+		// printf("read: %s\n read end\n", buff);
+	}
 	return (0);
 }
