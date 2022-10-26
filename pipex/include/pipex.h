4d0f93e811e2d767bb0ade2e310002ee96841f89
myoung jun ko <delphinus@Delphinuns-Laptop.localdomain>
2022-10-26 18:52:03 +0900

    update for linux

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:06:02 by myko              #+#    #+#             */
-/*   Updated: 2022/10/26 17:30:55 by myko             ###   ########.fr       */
+/*   Updated: 2022/10/26 18:49:54 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 # include <unistd.h> // fork, waitpid, execve, dup, dup2, pipe
 # include <stdlib.h> // exit
 # include <fcntl.h> // open, write
+
+#include <sys/wait.h> // wait for linux
 
 typedef enum s_erroridx {
 	ARGC_ERROR = 0,
