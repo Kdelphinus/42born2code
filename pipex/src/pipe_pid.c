dc26139891f2f8f61834e2754cbc4ba6a8da318d
Kdelphinus <kmj951015@gmail.com>
2022-11-04 02:58:48 +0900

    fix norminette

--- pipex/src/pipe_pid.c
+++ pipex/src/pipe_pid.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 14:06:53 by myko              #+#    #+#             */
-/*   Updated: 2022/11/04 01:51:35 by myko             ###   ########.fr       */
+/*   Updated: 2022/11/04 02:57:43 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,32 @@ static char	**exception(int i, t_envp tenvp, char *cmd)
 	new_argv[0] = ft_strdup(cmd);
 	new_argv[1] = ft_strdup(tenvp.argv[i] + ft_strlen(cmd));
 	new_argv[1] = ft_strtrim(new_argv[1], " ");
+	new_argv[2] = NULL;
 	if (ft_strncmp(cmd, "sh", 2) == 0)
 	{
-		if (access(new_argv[1], X_OK) == -1 && access(new_argv[1], F_OK) == 0)
+		if (access(new_argv[1], X_OK) == -1 \
+			&& access(new_argv[1], F_OK) == 0)
 			error(PERMISSION_ERROR, new_argv[1]);
 	}
-	new_argv[2] = NULL;
 	check_str(new_argv);
+	return (new_argv);
+}
+
+static char	**argv_init(int i, t_envp tenvp)
+{
+	char	**new_argv;
+
+	if (ft_strncmp(tenvp.argv[i], "awk", 3) == 0)
+		new_argv = exception(i, tenvp, "awk");
+	else if (ft_strncmp(tenvp.argv[i], "sed", 3) == 0)
+		new_argv = exception(i, tenvp, "sed");
+	else if (ft_strncmp(tenvp.argv[i], "./", 2) == 0)
+		new_argv = exception(i, tenvp, "sh");
+	else
+	{
+		new_argv = ft_split(tenvp.argv[i], ' ');
+		check_str(new_argv);
+	}
 	return (new_argv);
 }
 
@@ -54,29 +73,16 @@ void	child_pid(int fds[], t_envp tenvp)
 	fd = open(tenvp.argv[1], O_RDONLY);
 	if (fd == -1)
 		error(ERROR, tenvp.argv[1]);
-	if (dup2(fd, STDIN_FILENO) == -1)
-		error(ERROR, "fd");
-	if (dup2(fds[1], STDOUT_FILENO) == -1)
+	if (dup2(fd, STDIN_FILENO) == -1 || dup2(fds[1], STDOUT_FILENO) == -1)
 		error(ERROR, "fd");
 	close(fds[0]);
-	if (ft_strncmp(tenvp.argv[2], "awk", 3) == 0)
-		new_argv = exception(2, tenvp, "awk");
-	else if (ft_strncmp(tenvp.argv[2], "sed", 3) == 0)
-		new_argv = exception(2, tenvp, "sed");
-	else if (ft_strncmp(tenvp.argv[2], "./", 2) == 0)
-		new_argv = exception(2, tenvp, "sh");
-	else
-	{
-		new_argv = ft_split(tenvp.argv[2], ' ');
-		check_str(new_argv);
-	}
+	new_argv = argv_init(2, tenvp);
 	path = ft_path(new_argv[0], tenvp.paths);
 	if (!path)
 		error(COMMAND_ERROR, new_argv[0]);
 	if (execve(path, new_argv, tenvp.envp) == -1)
 		error(ERROR, new_argv[0]);
 }
-
 
 void	parent_pid(int fds[], t_envp tenvp)
 {
@@ -87,22 +93,10 @@ void	parent_pid(int fds[], t_envp tenvp)
 	fd = open(tenvp.argv[4], O_RDWR | O_CREAT | O_TRUNC, 420);
 	if (fd == -1)
 		error(ERROR, "fd");
-	if (dup2(fds[0], STDIN_FILENO) == -1)
-		error(ERROR, "fd");
-	if (dup2(fd, STDOUT_FILENO) == -1)
+	if (dup2(fds[0], STDIN_FILENO) == -1 || dup2(fd, STDOUT_FILENO) == -1)
 		error(ERROR, "fd");
 	close(fds[1]);
-	if (ft_strncmp(tenvp.argv[3], "awk", 3) == 0)
-		new_argv = exception(3, tenvp, "awk");
-	else if (ft_strncmp(tenvp.argv[3], "sed", 3) == 0)
-		new_argv = exception(3, tenvp, "sed");
-	else if (ft_strncmp(tenvp.argv[3], "./", 2) == 0)
-		new_argv = exception(3, tenvp, "sh");
-	else
-	{
-		new_argv = ft_split(tenvp.argv[3], ' ');
-		check_str(new_argv);
-	}
+	new_argv = argv_init(3, tenvp);
 	path = ft_path(new_argv[0], tenvp.paths);
 	if (!path)
 		error(COMMAND_ERROR, new_argv[0]);
--- pipex/src/utils.c
+++ pipex/src/utils.c
@@ -6,23 +6,22 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 14:05:31 by myko              #+#    #+#             */
-/*   Updated: 2022/11/04 01:57:00 by myko             ###   ########.fr       */
+/*   Updated: 2022/11/04 02:54:07 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-// TODO 우선 그냥 바로 할당
 static char	**path_init(void)
 {
 	char	**paths;
 
 	paths = malloc(sizeof(char *) * 6);
-	paths[0] = "/usr/local/bin";
-	paths[1] = "/usr/bin";
-	paths[2] = "/bin";
-	paths[3] = "/usr/sbin";
-	paths[4] = "/sbin";
+	paths[0] = ft_strdup("/usr/local/bin");
+	paths[1] = ft_strdup("/usr/bin");
+	paths[2] = ft_strdup("/bin");
+	paths[3] = ft_strdup("/usr/sbin");
+	paths[4] = ft_strdup("/sbin");
 	paths[5] = NULL;
 	return (paths);
 }
