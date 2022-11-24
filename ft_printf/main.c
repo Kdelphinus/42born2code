101957c1c6f537bf9f624faac2b4eb011485a4e8
Kdelphinus <kmj951015@gmail.com>
2022-11-24 17:15:30 +0900

    update main

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 21:37:26 by myko              #+#    #+#             */
-/*   Updated: 2022/08/12 22:54:18 by myko             ###   ########.fr       */
+/*   Updated: 2022/11/24 17:14:51 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	main(void)
 	ft_cnt = 0;
 	cnt = 0;
 	ft_printf("----test----\n");
-	ft_cnt = ft_printf("%d%s %c%s %X %i", 42, "Seoul", 'K', "delphinus", 42, 0x2a);
+	ft_cnt = ft_printf("%d%s %c%s %X %i", 42, "Seoul", 'K', "delphinus", -2147483647, 0x2a);
 	printf("\n");
-	cnt = printf("%d%s %c%s %X %i", 42, "Seoul", 'K', "delphinus", 42, 0x2a);
+	cnt = printf("%d%s %c%s %X %i", 42, "Seoul", 'K', "delphinus", -2147483647, 0x2a);
 	printf("\n");
 	printf("ft_printf cnt: %d\n", ft_cnt);
 	printf("printf cnt: %d\n", cnt);
