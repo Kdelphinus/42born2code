cf38706d75bc1b66bc22db08a1ff3275ac14004b
Kdelphinus <kmj951015@gmail.com>
2022-12-20 21:19:17 +0900

    check stack

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,16 +6,16 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 14:01:02 by myko              #+#    #+#             */
-/*   Updated: 2022/12/20 16:18:58 by myko             ###   ########.fr       */
+/*   Updated: 2022/12/20 21:18:43 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
+#include <stdio.h> // TODO 스택 확인을 위한 헤더
 
 int	main(int argc, char **argv)
 {
 	t_stack	stack;
-	t_llist	orders;
 
 	if (argc == 1)
 		return (0);
@@ -35,6 +35,11 @@ int	main(int argc, char **argv)
 	if (!stack.b)
 		ft_error();
 	stack.b_len = 0;
+
+	// TODO 스택에 잘 들어왔는지 확인, 나중에 지우기
+	for (int i = 0; i < stack.a_len; i++)
+		printf("%d ", stack.a[i]);
+	printf("\n");
 	stack_sort(&stack);
 	return (0);
 }
