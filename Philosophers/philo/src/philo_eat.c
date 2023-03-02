284933f9fdae00545e09e78f110c5795439ab38a
Kdelphinus <kmj951015@gmail.com>
2023-03-02 18:31:36 +0900

    죽으면 안 되는 경우가 밀려서 죽는 경우가 생김

--- Philosophers/philo/src/philo_eat.c
+++ Philosophers/philo/src/philo_eat.c
@@ -6,7 +6,7 @@
 /*   By: myko <myko@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 18:47:08 by myko              #+#    #+#             */
-/*   Updated: 2023/03/02 17:50:12 by myko             ###   ########.fr       */
+/*   Updated: 2023/03/02 18:31:11 by myko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,6 @@ int	philo_eat(t_dining *dining, t_philo *philo, int id)
 			dining->forks[philo->right_fork] = USING;
 			philo_print(dining, "has taken a fork", id);
 			philo_print(dining, "has taken a fork", id);
-//			pthread_mutex_unlock(&dining->pick_up[philo->right_fork]);
-//			pthread_mutex_unlock(&dining->pick_up[philo->left_fork]);
 			break ;
 		}
 		dining->forks[philo->left_fork] = NOT_USING;
@@ -52,10 +50,8 @@ int	philo_eat(t_dining *dining, t_philo *philo, int id)
 	philo->last_eat = get_time();
 	philo->eat_cnt++;
 	eating(dining);
-//	pthread_mutex_lock(&dining->pick_up[philo->right_fork]);
 	dining->forks[philo->right_fork] = NOT_USING;
 	pthread_mutex_unlock(&dining->pick_up[philo->right_fork]);
-//	pthread_mutex_lock(&dining->pick_up[philo->left_fork]);
 	dining->forks[philo->left_fork] = NOT_USING;
 	pthread_mutex_unlock(&dining->pick_up[philo->left_fork]);
 	return (dining->eat_flag);
