d8c76d4c5d839469e7453d66719e3937a794ff56
Kdelphinus <kmj951015@gmail.com>
2023-04-25 19:43:58 +0900

    ex00 | 해결

---
+++ cpp_module/module_00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	std::string str;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j]; j++)
+				str += (char)std::toupper(argv[i][j]);
+		}
+		std::cout << str << std::endl;
+	}
+	return (0);
+}
