894c50cd66bb81318a83ee7271a95c27e6f195d1
Kdelphinus <kmj951015@gmail.com>
2023-05-25 18:50:02 +0900

    [module 02] README.md 소수점 처리 추가

--- cpp_module/module_02/ex00/Fixed.cpp
+++ cpp_module/module_02/ex00/Fixed.cpp
@@ -1,1 +1,19 @@
 #include "Fixed.hpp"
+
+Fixed::Fixed() : _num(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) : {
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+    std::cout << "Copy assignment operator called" << std::endl;
+
+    return (*this);
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
--- cpp_module/module_02/ex00/Fixed.hpp
+++ cpp_module/module_02/ex00/Fixed.hpp
@@ -1,4 +1,17 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+class Fixed {
+	private:
+		int _num;
+		static const int _fractionalBits = 8;
+    public:
+        Fixed();
+        Fixed(const Fixed &fixed);
+        Fixed &Fixed::operator=(const Fixed &fixed);
+        ~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
 #endif
