a2685995f196b2c2552811081103de22000ea6d2
Kdelphinus <kmj951015@gmail.com>
2023-05-27 21:00:45 +0900

    [module 02/ex01] 생성

---
+++ cpp_module/module_02/ex01/Fixed.hpp
@@ -0,0 +1,19 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int _num;
+		static const int _fractionalBits = 8;
+    public:
+        Fixed();
+        Fixed(const Fixed &fixed);
+        Fixed &operator=(const Fixed &fixed);
+        ~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+#endif
