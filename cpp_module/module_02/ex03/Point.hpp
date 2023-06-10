7dea817e9453848715b45bde0b5bb8eb32422315
Kdelphinus <kmj951015@gmail.com>
2023-06-10 21:02:44 +0900

    [module 02/ex03] Point class 생성 및 bsp 프로토타입 선언

--- cpp_module/module_02/ex03/Point.hpp
+++ cpp_module/module_02/ex03/Point.hpp
@@ -1,7 +1,12 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
+#include "Fixed.hpp"
+
 class Point {
+    private:
+        Fixed const x;
+        Fixed const y;
     public:
         Point();
         Point(const Point &point);
---
+++ cpp_module/module_02/ex03/bsp.cpp
@@ -0,0 +1,5 @@
+#include "Point.hpp"
+
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+
+}
