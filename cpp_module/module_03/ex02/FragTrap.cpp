0b74fb4501ec1ba1d7595c996c1c13954a5e7d45
Kdelphinus <kmj951015@gmail.com>
2023-06-10 21:47:33 +0900

    [module 03] 생성, 소멸자 호출 시 이름도 같이 출력되도록 변경

--- cpp_module/module_03/ex02/FragTrap.cpp
+++ cpp_module/module_03/ex02/FragTrap.cpp
@@ -4,7 +4,7 @@ FragTrap::FragTrap() : ClapTrap() {
     this->_hitPoints = 100;
     this->_energyPoints = 100;
     this->_attackDamage = 30;
-    std::cout << "FragTrap Default constructor called" << std::endl;
+    std::cout << " FragTrap " << this->_name << " Default constructor called" << std::endl;
 }
 
 FragTrap::FragTrap(std::string name) {
@@ -12,7 +12,7 @@ FragTrap::FragTrap(std::string name) {
     this->_hitPoints = 100;
     this->_energyPoints = 100;
     this->_attackDamage = 30;
-    std::cout << "FragTrap Name constructor called" << std::endl;
+    std::cout << "FragTrap " << this->_name << " Name constructor called" << std::endl;
 }
 
 FragTrap::FragTrap(const FragTrap &ft) {
@@ -20,22 +20,22 @@ FragTrap::FragTrap(const FragTrap &ft) {
     this->_hitPoints = ft.getHitPoints();
     this->_energyPoints = ft.getEnergyPoints();
     this->_attackDamage = ft.getDamage();
-    std::cout << "FragTrap Copy constructor called" << std::endl;
+    std::cout << "FragTrap " << this->_name << " Copy constructor called" << std::endl;
 }
 
 FragTrap &FragTrap::operator=(const FragTrap &ft) {
-    std::cout << "FragTrap Copy assignment operator called" << std::endl;
     if (this != &ft) {
         this->_name = ft.getName();
         this->_hitPoints = ft.getHitPoints();
         this->_energyPoints = ft.getEnergyPoints();
         this->_attackDamage = ft.getDamage();
     }
+    std::cout << "FragTrap Copy assignment operator called" << std::endl;
     return *this;
 }
 
 FragTrap::~FragTrap() {
-    std::cout << "FragTrap Destructor called" << std::endl;
+    std::cout << "FragTrap " << this->_name << " Destructor called" << std::endl;
 }
 
 void FragTrap::highFivesGuys() {
