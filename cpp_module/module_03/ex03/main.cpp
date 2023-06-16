17cd76e3983e33ad8c989ef982cc71326f712214
Kdelphinus <kmj951015@gmail.com>
2023-06-16 15:38:38 +0900

    [module 03/ex03] -Wshadow -Wno-shadow 플래그와 테스트 추가

--- cpp_module/module_03/ex03/main.cpp
+++ cpp_module/module_03/ex03/main.cpp
@@ -10,4 +10,13 @@ int main() {
               << "hp: " << a.getHitPoints() << " (FragTrap: 100)\n"
               << "ep: " << a.getEnergyPoints() << " (ScavTrap: 50)\n"
               << "damage: " << a.getDamage() << " (FragTrap: 30)" << std::endl;
+
+    int num;
+    num = 3;
+    std::cout << num << std::endl;
+    {
+        int num;
+        num = 5;
+        std::cout << num << std::endl;
+    }
 }
