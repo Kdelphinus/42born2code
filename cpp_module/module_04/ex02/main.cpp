24bb72ab6f1110f955b8219b1940c53a7c52733f
Kdelphinus <kmj951015@gmail.com>
2023-06-20 14:34:10 +0900

    [module 04/ex02] Makefile src 추가 및 테스트 추가

--- cpp_module/module_04/ex02/main.cpp
+++ cpp_module/module_04/ex02/main.cpp
@@ -1,5 +1,7 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "AbstractCat.hpp"
+#include "AbstractDog.hpp"
 
 /*
  * Animal이 const로 선언되었기에 Animal에서 선언된 멤버들은 수정이 불가하다.
@@ -12,25 +14,61 @@
 */
 
 int main() {
-    const Animal *dog = new Dog();
-    const Animal *cat = new Cat();
+//    추상 클래스이기에 컴파일 시, 에러
+//    const AbstractAnimal *abstractAnimal = new AbstractAnimal();
+
+    std::cout << "\n========ANIMAL========" << std::endl;
+    std::cout << "<create>" << std::endl;
+    const Animal *animal = new Animal();
+    std::cout << "\ntype : " << animal->getType() << std::endl;
+    std::cout << "sound: ";
+    animal->makeSound();
+    std::cout << "\n<delete>" << std::endl;
+    delete animal;
 
     std::cout << "\n========DOG========" << std::endl;
-    std::cout << "type : " << dog->getType() << std::endl;
+    std::cout << "<create>" << std::endl;
+    const Animal *dog = new Dog();
+    std::cout << "\ntype : " << dog->getType() << std::endl;
     std::cout << "sound: ";
     dog->makeSound();
     dog->setIdeas(0, "Dog's idea");
     std::cout << "ideas: " << dog->getIdeas(0) << std::endl;
+    std::cout << "\n<delete>" << std::endl;
+    delete dog;
 
     std::cout << "\n========CAT========" << std::endl;
-    std::cout << "type : " << cat->getType() << std::endl;
+    std::cout << "<create>" << std::endl;
+    const Animal *cat = new Cat();
+    std::cout << "\ntype : " << cat->getType() << std::endl;
     std::cout << "sound: ";
     cat->makeSound();
-    cat->setIdeas(0, "cat's idea");
+    cat->setIdeas(0, "Cat's idea");
     std::cout << "ideas: " << cat->getIdeas(0) << std::endl;
-    std::cout << std::endl;
-
-    delete dog;
+    std::cout << "\n<delete>" << std::endl;
     delete cat;
+
+    std::cout << "\n========ABSTRACT DOG========" << std::endl;
+    std::cout << "<create>" << std::endl;
+    const AbstractAnimal *abstractDog = new AbstractDog();
+    std::cout << "\ntype : " << abstractDog->getType() << std::endl;
+    std::cout << "sound: ";
+    abstractDog->makeSound();
+    abstractDog->setIdeas(0, "Abstract Dog's idea");
+    std::cout << "ideas: " << abstractDog->getIdeas(0) << std::endl;
+    std::cout << "\n<delete>" << std::endl;
+    delete abstractDog;
+
+    std::cout << "\n========ABSTRACT CAT========" << std::endl;
+    std::cout << "<create>" << std::endl;
+    const AbstractAnimal *abstractCat = new AbstractCat();
+    std::cout << "\ntype : " << abstractCat->getType() << std::endl;
+    std::cout << "sound: ";
+    abstractCat->makeSound();
+    abstractCat->setIdeas(0, "Abstract Cat's idea");
+    std::cout << "ideas: " << abstractCat->getIdeas(0) << std::endl;
+    std::cout << "\n<delete>" << std::endl;
+    delete abstractCat;
+
     return 0;
 }
