a4072bfc101165e356d81ab340e9bd6b1675ee70
Kdelphinus <kmj951015@gmail.com>
2023-07-03 03:57:15 +0900

    [module 01/ex04] sed 테스트구문 삭제

--- cpp_module/module_01/ex04/sed.cpp
+++ cpp_module/module_01/ex04/sed.cpp
@@ -10,8 +10,6 @@ std::string my_sed(std::string origin, std::string s1, std::string s2) {
 			repl += s2;
 		else
 			repl += origin.substr(start, pos) + s2;
-		std::cout << "start: " << start << " pos: " << pos << std::endl;
-		std::cout << origin.substr(start, pos - start) << std::endl;
 		start += pos + s1.size();
 		pos = origin.substr(start, origin.size()).find(s1);
 	}
