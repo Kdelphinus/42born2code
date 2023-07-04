c36d20d30f78d286ba9bb01b5c33cc83b5ca65d4
Kdelphinus <kmj951015@gmail.com>
2023-07-04 20:55:39 +0900

    [module 02/ex03] 논리 구조 수정

--- cpp_module/module_02/ex03/bsp.cpp
+++ cpp_module/module_02/ex03/bsp.cpp
@@ -6,22 +6,16 @@
  * 같은 사이드에 있는지 확인하면 삼각형 내부에 점이 있는지 확인할 수 있다.
  */
 
-static int lineEquation(Point const p, Point const a, Point const b) {
+static bool lineEquation(Point const p, Point const a, Point const b) {
     Fixed zero = Fixed();
-    Fixed ans = (p.getX() - a.getX()) * (a.getY() - b.getY()) - (p.getY() - a.getY()) * (a.getX() - b.getX());
+    Fixed ans = (b.getX() - a.getX()) * (p.getY() - b.getY()) - (b.getY() - a.getY()) * (p.getX() - b.getX());
     if (ans > zero)
-        return 1;
-    else if (ans < zero)
-        return -1;
-    return 0;
+        return true;
+    return false;
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
-    if (lineEquation(point, a, b) != lineEquation(c, a, b) || lineEquation(point, a, b) == 0)
-        return false;
-    if (lineEquation(point, a, c) != lineEquation(b, a, c) || lineEquation(point, a, c) == 0)
-        return false;
-    if (lineEquation(point, b, c) != lineEquation(a, b, c) || lineEquation(point, b, c) == 0)
-        return false;
-    return true;
+    if (lineEquation(point, a, b) && lineEquation(point, b, c) && lineEquation(point, c, a))
+        return true;
+    return false;
 }
--- cpp_module/module_02/ex03/main.cpp
+++ cpp_module/module_02/ex03/main.cpp
@@ -6,13 +6,19 @@ int main() {
     Point b = Point(Fixed(1), Fixed(-1));
     Point c = Point(Fixed(0), Fixed(1));
 
+    std::cout << std::boolalpha;
+
     Point p1 = Point(Fixed(0), Fixed(0));
-    std::cout << "inside of a triangle: " << bsp(a, b, c, p1) << std::endl;
+    std::cout <<  "inside of a triangle: " << bsp(a, b, c, p1) << std::endl;
 
     Point p2 = Point(Fixed(0), Fixed(-1));
     std::cout << "boundary of a triangle: " << bsp(a, b, c, p2) << std::endl;
 
     Point p3 = Point(Fixed(1), Fixed(0));
     std::cout << "outside of a triangle: " << bsp(a, b, c, p3) << std::endl;
+
+    Point p4 = Point(Fixed(-1), Fixed(1));
+    std::cout << "outside of a triangle: " << bsp(a, b, c, p4) << std::endl;
+
     return 0;
 }
