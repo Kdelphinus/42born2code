e115f02d6076eb8d680dd0d4e80b628f97af25dc
Kdelphinus <kmj951015@gmail.com>
2023-07-04 21:47:57 +0900

    [module 02/ex03] 조건문 수정

--- cpp_module/module_02/ex03/main.cpp
+++ cpp_module/module_02/ex03/main.cpp
@@ -2,13 +2,13 @@
 #include "Point.hpp"
 
 int main() {
-    Point a = Point(Fixed(-1), Fixed(-1));
-    Point b = Point(Fixed(1), Fixed(-1));
-    Point c = Point(Fixed(0), Fixed(1));
+    Point a = Point(Fixed(1), Fixed(5));
+    Point b = Point(Fixed(5), Fixed(1));
+    Point c = Point(Fixed(5), Fixed(5));
 
     std::cout << std::boolalpha;
 
-    Point p1 = Point(Fixed(0), Fixed(0));
+    Point p1 = Point(Fixed(4), Fixed(4));
     std::cout <<  "inside of a triangle: " << bsp(a, b, c, p1) << std::endl;
 
     Point p2 = Point(Fixed(0), Fixed(-1));
