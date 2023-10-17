84c6ae28d6f4b2c496963ca8edeb65889e6d681e
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-17 20:56:11 +0900

    :art: 레퍼런스를 사용하는 함수문을 try, catch문으로 변경
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_06/ex02/main.cpp
+++ cpp_module/module_06/ex02/main.cpp
@@ -5,11 +5,17 @@
 Base *generate(void) {
 	std::srand(std::time(0));
 	int i = std::rand() % 3;
-	switch (i) {
-		case 0: return new A();
-		case 1: return new B();
-		case 2: return new C();
-		default: return new A();
+
+	std::cout << "Create: ";
+	if (i == 0) {
+		std::cout << "Type A" << std::endl;
+		return new A();
+	} else if (i == 1) {
+		std::cout << "Type B" << std::endl;
+		return new B();
+	} else {
+		std::cout << "Type C" << std::endl;
+		return new C();
 	}
 }
 
@@ -24,15 +30,26 @@ void identify(Base *p) {
 	return;
 }
 
-// TODO : dynamic_cast 안에 <A *>는 포인터를 쓰지 않았다고 할 수 있는가?
 void identify(Base &p) {
 	std::cout << "Reference: ";
-	if (dynamic_cast<A *>(&p))
+	try {
+		A &a = dynamic_cast<A &>(p);
+		(void)a;
 		std::cout << "Type A" << std::endl;
-	else if (dynamic_cast<B *>(&p))
+	}
+	catch (...) {}
+	try {
+		B &b = dynamic_cast<B &>(p);
+		(void)b;
 		std::cout << "Type B" << std::endl;
-	else if (dynamic_cast<C *>(&p))
+	}
+	catch (...) {}
+	try {
+		C &c = dynamic_cast<C &>(p);
+		(void)c;
 		std::cout << "Type C" << std::endl;
+	}
+	catch (...) {}
 }
 
 int main() {
