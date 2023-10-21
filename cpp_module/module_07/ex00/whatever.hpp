34345178c42208a4781accd9f79c06968b0722ea
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-21 18:54:29 +0900

    :sparkles: ex00 템플릿 구현

---
+++ cpp_module/module_07/ex00/whatever.hpp
@@ -0,0 +1,24 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T>
+T swap(T &a, T &b) {
+  T tmp = a;
+  a = b;
+  b = tmp;
+  return (a);
+}
+
+template<typename T>
+T min(T a, T b) {
+  return (a < b ? a : b);
+}
+
+template<typename T>
+T max(T a, T b) {
+  return (a > b ? a : b);
+}
+
+#endif
