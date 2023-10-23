36723b0cc7d68c695b6103cd0711afd3143c7481
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-23 13:24:40 +0900

    :sparkles: 템플릿 함수 iter에 대한 테스트 작성

--- cpp_module/module_07/ex01/main.cpp
+++ cpp_module/module_07/ex01/main.cpp
@@ -1,5 +1,37 @@
 #include "iter.hpp"
 
+template <typename T>
+void print(T& x) {
+	std::cout << x << " ";
+}
+
 int main() {
-  
+	char tab_char[] = {'a', 'b', 'c', 'd', 'e'};
+	int tab[] = {0, 1, 2,3, 4};
+	double tab_double[] = {0.0, 1.1, 2.2, 3.3, 4.4};
+
+	std::cout << "iter(tab_char, 5, print<const char>): ";
+	iter(tab_char, 5, print<const char>);
+	std::cout << std::endl;
+
+	std::cout << "iter(tab_char, 5, print<char>): ";
+	iter(tab_char, 5, print<char>);
+	std::cout << std::endl;
+
+	std::cout << "iter(tab, 5, print<const int>): ";
+	iter(tab, 5, print<const int>);
+	std::cout << std::endl;
+
+	std::cout << "iter(tab, 5, print<int>): ";
+	iter(tab, 5, print<int>);
+	std::cout << std::endl;
+
+	std::cout << "iter(tab_double, 5, print<const double>): ";
+	iter(tab_double, 5, print<const double>);
+	std::cout << std::endl;
+
+	std::cout << "iter(tab_double, 5, print<double>): ";
+	iter(tab_double, 5, print<double>);
+	std::cout << std::endl;
+	return 0;
 }
