3b7eda1e2e552ba6ac0f5d4634088a859fae6ff0
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-23 13:24:21 +0900

    :sparkles: iter 구현

--- cpp_module/module_07/ex01/iter.hpp
+++ cpp_module/module_07/ex01/iter.hpp
@@ -4,5 +4,17 @@
 #include <iostream>
 
 template<typename T>
+void iter(T *array, size_t length, void (*f)(T const &)) {
+	for (size_t i = 0; i < length; i++) {
+		f(array[i]);
+	}
+}
+
+template<typename T>
+void iter(T *array, size_t length, void (*f)(T &)) {
+	for (size_t i = 0; i < length; i++) {
+		f(array[i]);
+	}
+}
 
 #endif
