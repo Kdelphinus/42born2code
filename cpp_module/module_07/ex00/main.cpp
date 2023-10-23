c0ff3c50262f8a0fcf5469b99b58134f3ec08d3c
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-23 13:23:30 +0900

    :adhesive_bandage: 함수 호출 수정

--- cpp_module/module_07/ex00/main.cpp
+++ cpp_module/module_07/ex00/main.cpp
@@ -4,7 +4,7 @@ int main() {
   int a = 2;
   int b = 3;
 
-  ::swap(a, b);
+  swap(a, b);
   std::cout << "a = " << a << ", b = " << b << std::endl;
   std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
   std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
@@ -12,7 +12,7 @@ int main() {
   std::string c = "chaine1";
   std::string d = "chaine2";
 
-  ::swap(c, d);
+  swap(c, d);
   std::cout << "c = " << c << ", d = " << d << std::endl;
   std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
   std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
