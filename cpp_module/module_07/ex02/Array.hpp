8708a8222757d959534d88a0c715b9428c0fa941
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-23 15:17:38 +0900

    :sparkles: 템플릿 클래스 Array 구현

---
+++ cpp_module/module_07/ex02/Array.hpp
@@ -0,0 +1,39 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template<typename T>
+class Array {
+ private:
+  T *_array;
+  unsigned int _size;
+ public:
+  Array() : _array(new T()), _size(0) {};
+  Array(unsigned int n) : _array(new T[n]), _size(n) {};
+  Array(const Array &src) : _array(new T[src._size]), _size(src._size) {
+	for (unsigned int i = 0; i < _size; i++)
+	  _array[i] = src._array[i];
+  };
+  Array &operator=(const Array &src) {
+	if (this != &src) {
+	  delete[] _array;
+	  _array = new T[src._size];
+	  _size = src._size;
+	  for (unsigned int i = 0; i < _size; i++)
+		_array[i] = src._array[i];
+	  return *this;
+	}
+  };
+  ~Array() { delete[] _array; };
+
+  T &operator[](unsigned int i) {
+	if (i >= _size)
+	  throw std::exception();
+	return _array[i];
+  }
+
+  unsigned int size() const { return _size; };
+};
+
+#endif
