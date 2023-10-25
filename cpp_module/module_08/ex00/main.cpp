9027457a8d0c17b90c7cc4f84a4e216943a31820
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-25 21:24:27 +0900

    :art: 연관 컨테이너인 set 제거 및 출력문 수정
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_08/ex00/main.cpp
+++ cpp_module/module_08/ex00/main.cpp
@@ -1,65 +1,57 @@
 #include <vector>
 #include <list>
 #include <deque>
-#include <set>
+
 #include "easyfind.hpp"
 
 int main() {
-  std::cout << "\nVector" << std::endl;
+  std::cout << "<Vector>" << std::endl;
   std::vector<int> v;
-  v.push_back(1);
-  v.push_back(2);
   v.push_back(3);
+  v.push_back(2);
+  v.push_back(1);
+  v.push_back(0);
   try {
-	std::cout << *easyfind(v, 2) << std::endl;
-	std::cout << *easyfind(v, 3) << std::endl;
-	std::cout << *easyfind(v, 1) << std::endl;
-	std::cout << *easyfind(v, 4) << std::endl;
+	for (int i = 0; i < 5; i++) {
+	  std::vector<int>::iterator it = easyfind(v, i);
+	  std::cout << "v[" << std::distance(v.begin(), it) << "] = " << *it
+				<< std::endl;
+	}
   } catch (std::exception &e) {
 	std::cout << "Not found" << std::endl;
   }
 
-  std::cout << "\nList" << std::endl;
+  std::cout << "\n<List>" << std::endl;
   std::list<int> l;
-  l.push_back(1);
-  l.push_back(2);
   l.push_back(3);
+  l.push_back(2);
+  l.push_back(1);
+  l.push_back(0);
   try {
-	std::cout << *easyfind(l, 2) << std::endl;
-	std::cout << *easyfind(l, 3) << std::endl;
-	std::cout << *easyfind(l, 1) << std::endl;
-	std::cout << *easyfind(l, 4) << std::endl;
+	for (int i = 0; i < 5; i++) {
+	  std::list<int>::iterator it = easyfind(l, i);
+	  std::cout << "l[" << std::distance(l.begin(), it) << "] = " << *it
+				<< std::endl;
+	}
   } catch (std::exception &e) {
 	std::cout << "Not found" << std::endl;
   }
 
   std::cout << "\nDeque" << std::endl;
   std::deque<int> d;
-  d.push_back(1);
-  d.push_back(2);
   d.push_back(3);
+  d.push_back(2);
+  d.push_back(1);
+  d.push_back(0);
   try {
-	std::cout << *easyfind(d, 2) << std::endl;
-	std::cout << *easyfind(d, 3) << std::endl;
-	std::cout << *easyfind(d, 1) << std::endl;
-	std::cout << *easyfind(d, 4) << std::endl;
+	for (int i = 0; i < 5; i++) {
+	  std::deque<int>::iterator it = easyfind(d, i);
+	  std::cout << "d[" << std::distance(d.begin(), it) << "] = " << *it
+				<< std::endl;
+	}
   } catch (std::exception &e) {
 	std::cout << "Not found" << std::endl;
   }
 
-  std::cout << "\nSet" << std::endl;
-  std::set<int> s;
-  s.insert(1);
-  s.insert(2);
-  s.insert(3);
-  try {
-	std::cout << *easyfind(s, 2) << std::endl;
-	std::cout << *easyfind(s, 3) << std::endl;
-	std::cout << *easyfind(s, 1) << std::endl;
-	std::cout << *easyfind(s, 4) << std::endl;
-  }
-  catch (std::exception &e) {
-	std::cout << "Not found" << std::endl;
-  }
   return 0;
 }
