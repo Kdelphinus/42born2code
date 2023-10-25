0125dfba00713b6de0b84a3abf7da63bcf1ab5aa
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-25 23:05:30 +0900

    :adhesive_bandage: 테스트할 출력문들 추가
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_08/ex01/main.cpp
+++ cpp_module/module_08/ex01/main.cpp
@@ -1,16 +1,21 @@
 #include "Span.hpp"
 
 int main() {
-  Span sp = Span(5);
+  try {
+	Span sp = Span(10);
 
-  sp.addNumber(6);
-  sp.addNumber(3);
-  sp.addNumber(17);
-  sp.addNumber(9);
-  sp.addNumber(11);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	sp.addNumber(11);
 
-  std::cout << sp.shortestSpan() << std::endl;
-  std::cout << sp.longestSpan() << std::endl;
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+  } catch (std::exception &e) {
+	std::cout << e.what() << std::endl;
+  }
 
   return 0;
 }
