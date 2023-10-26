b99a2e48015f496ba2edd0d36ae2008ccb9897f7
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-26 15:16:42 +0900

    :sparkles: MutanStack 구현
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_08/ex02/MutantStack.hpp
+++ cpp_module/module_08/ex02/MutantStack.hpp
@@ -1,4 +1,39 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+ public:
+  MutantStack() {};
+  MutantStack(MutantStack const &stack) { *this = stack; };
+  MutantStack &operator=(MutantStack const &stack) {
+	if (this != &stack) {
+	  this->c = stack.c;
+	}
+	return *this;
+  }
+  ~MutantStack() {};
+
+  typedef typename std::stack<T>::container_type::iterator iterator;
+  iterator begin() { return this->c.begin(); }
+  iterator end() { return this->c.end(); }
+
+  typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+  const_iterator begin() const { return this->c.begin(); }
+  const_iterator end() const { return this->c.end(); }
+
+  typedef typename std::stack<T>::container_type::reverse_iterator
+	  reverse_iterator;
+  reverse_iterator rbegin() { return this->c.rbegin(); }
+  reverse_iterator rend() { return this->c.rend(); }
+
+  typedef typename std::stack<T>::container_type::const_reverse_iterator
+	  const_reverse_iterator;
+  const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+  const_reverse_iterator rend() const { return this->c.rend(); }
+};
+
 #endif
