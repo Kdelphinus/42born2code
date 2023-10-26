6aea3914e892d75afb34f44cddd8977a832b55b6
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-26 15:16:13 +0900

    :white_check_mark: 각 iterator에 대한 테스트 및 list와 비교 테스트 추가
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_08/ex02/main.cpp
+++ cpp_module/module_08/ex02/main.cpp
@@ -1,32 +1,84 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main() {
-  MutantStack<int> mstack;
+  {
+	MutantStack<int> mstack;
 
-  mstack.push(5);
-  mstack.push(17);
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "mstack top: " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "mstack size: " << mstack.size() << std::endl;
 
-  std::cout << mstack.top() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
 
-  mstack.pop();
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
 
-  std::cout << mstack.size() << std::endl;
-
-  mstack.push(3);
-  mstack.push(5);
-  mstack.push(737);
-  //[...]
-  mstack.push(0);
-
-  MutantStack<int>::iterator it = mstack.begin();
-  MutantStack<int>::iterator ite = mstack.end();
-
-  ++it;
-  --it;
-  while (it != ite) {
-	std::cout << *it << std::endl;
 	++it;
+	--it;
+	std::cout << "mstack: ";
+	while (it != ite) {
+	  std::cout << *it << " ";
+//	  *it = 100;  // iterator 타입은 값을 바꿀 수있다.
+	  ++it;
+	}
+	std::cout << std::endl;
+
+	MutantStack<int>::const_iterator cit = mstack.begin();
+	MutantStack<int>::const_iterator cend = mstack.end();
+	std::cout << "const: ";
+	while (cit != cend) {
+	  std::cout << *cit << " ";
+//	  *cit = 100;  // const_iterator 타입은 값을 바꿀 수 없다. compile error
+	  ++cit;
+	}
+	std::cout << std::endl;
+
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rend = mstack.rend();
+	std::cout << "reverse: ";
+	while (rit != rend) {
+	  std::cout << *rit << " ";
+	  ++rit;
+	}
+	std::cout << std::endl;
+
+	std::stack<int> s(mstack);
+	std::cout << "stack size: " << s.size() << std::endl;
+//	std::stack<int>::iterator sit = s.begin(); //  stack은 iterator가 없다.
   }
-  std::stack<int> s(mstack);
+
+  std::cout << "\n============================================\n" << std::endl;
+
+  {
+	std::list<int> list;
+
+	list.push_back(5);
+	list.push_back(17);
+	std::cout << "list back: " << list.back() << std::endl;
+	list.pop_back();
+	std::cout << "list size: " << list.size() << std::endl;
+
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+	std::list<int>::iterator it = list.begin();
+	std::list<int>::iterator ite = list.end();
+	++it;
+	--it;
+	std::cout << "list: ";
+	while (it != ite) {
+	  std::cout << *it << " ";
+	  ++it;
+	}
+	std::cout << std::endl;
+	std::list<int> s(list);
+  }
   return 0;
 }
