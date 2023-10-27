916860bd16efdf0145b76562c991dc33f14c5039
Myoung Jun Ko <kmj951015@gmail.com>
2023-10-27 17:19:05 +0900

    :adhesive_bandage: static 초기화 제거
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_06/ex00/main.cpp
+++ cpp_module/module_06/ex00/main.cpp
@@ -1,11 +1,5 @@
 #include "scalarConverter.hpp"
 
-char ScalarConverter::_char;
-int ScalarConverter::_int;
-float ScalarConverter::_float;
-double ScalarConverter::_double;
-bool ScalarConverter::_isPseudoLiteral = false;
-
 int main(int ac, char **av) {
 	if (ac != 2) {
 		std::cout << "Usage: ./convert [input]" << std::endl;
