cda7ffae40b558b7c9bb1c13659d31a3c054a2ea
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-01 13:22:39 +0900

    :art: 함수도 템플릿으로 사용하여 const를 따로 구현하지 않아도 됨

--- cpp_module/module_07/ex01/iter.hpp
+++ cpp_module/module_07/ex01/iter.hpp
@@ -3,18 +3,11 @@
 
 #include <iostream>
 
-template<typename T>
-void iter(T *array, size_t length, void (*f)(T const &)) {
-	for (size_t i = 0; i < length; i++) {
-		f(array[i]);
-	}
-}
-
-template<typename T>
-void iter(T *array, size_t length, void (*f)(T &)) {
-	for (size_t i = 0; i < length; i++) {
-		f(array[i]);
-	}
+template<typename T, typename FUNC>
+void iter(T *array, size_t length, FUNC f) {
+  for (size_t i = 0; i < length; i++) {
+	f(array[i]);
+  }
 }
 
 #endif
