dc98e2afc4c6258fd32a423aa2bef90dde901d2e
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-06 21:10:14 +0900

    :art: input의 타입을 확인하고 먼저 변환 후, 변환된 것으로 나머지 타입 변환
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_06/ex00/scalarConverter.cpp
+++ cpp_module/module_06/ex00/scalarConverter.cpp
@@ -4,6 +4,9 @@ char ScalarConverter::_char;
 int ScalarConverter::_int;
 float ScalarConverter::_float;
 double ScalarConverter::_double;
+bool ScalarConverter::_isImpossible;
+bool ScalarConverter::_isCharImpossible;
+bool ScalarConverter::_isIntImpossible;
 
 ScalarConverter::ScalarConverter() {}
 
@@ -16,14 +19,12 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &) {
 ScalarConverter::~ScalarConverter() {}
 
 const char *ScalarConverter::InvalidInput::what() const throw() {
-  return "Error: Invalid input";
+  return "char: impossible\nint: impossible\nfloat: nanf\ndouble: nan";
 }
 
 bool ScalarConverter::isPseudoLiteral(const std::string &input) {
   if (input == "+inf" || input == "-inf" || input == "inf" ||
-	  input == "+nan" || input == "-nan" || input == "nan" ||
-	  input == "+inff" || input == "-inff" || input == "inff" ||
-	  input == "+nanf" || input == "-nanf" || input == "nanf") {
+	  input == "+nan" || input == "-nan" || input == "nan") {
 	_double = std::atof(input.c_str());
 	std::cout << "char: impossible\n"
 			  << "int: impossible\n"
@@ -31,15 +32,32 @@ bool ScalarConverter::isPseudoLiteral(const std::string &input) {
 			  << "double: " << _double << std::endl;
 	return true;
   }
+  if (input == "+inff" || input == "-inff" || input == "inff" ||
+	  input == "+nanf" || input == "-nanf" || input == "nanf") {
+	_float = static_cast<float>(std::atof(input.c_str()));
+	std::cout << "char: impossible\n"
+			  << "int: impossible\n"
+			  << "float: " << _float << "\n"
+			  << "double: " << static_cast<double>(_float) << std::endl;
+	return true;
+  }
   return false;
+}
+
+TYPE ScalarConverter::getType(const std::string &input) {
+  if (input.find_first_of(".") != input.npos) {
+	if (input[input.size() - 1] == 'f')
+	  return FLOAT;
+	return DOUBLE;
+  }
+  if (input.size() == 1 && !std::isdigit(input[0]))
+	return CHAR;
+  return INT;
 }
 
 bool ScalarConverter::isNumeric(const std::string &input) {
   int dotCnt = 0;
   int numCnt = 0;
-
-  if (input.size() == 1 && !std::isdigit(input[0]))
-	return true;
 
   for (size_t i = 0; i < input.size(); ++i) {
 	if (i == 0 && (input[i] == '-' || input[i] == '+'))
@@ -56,69 +74,158 @@ bool ScalarConverter::isNumeric(const std::string &input) {
 	  return false;
 	++numCnt;
   }
-  if (numCnt == 0)
+  if (numCnt == 0 || (dotCnt == 0 && input[input.size() - 1] == 'f'))
 	return false;
   return true;
 }
 
-void ScalarConverter::convert(const std::string &input) {
-  // inf, nan일 경우
-  if (isPseudoLiteral(input))
-	return;
-
-  // 숫자가 아닌 경우
-  if (!isNumeric(input))
-	throw InvalidInput();
-
-  // 소수점 처리
-  int precision = 1;
-  if (input.find_first_of(".") != input.npos && input[input.size() - 1] != '.')
-	precision = input.size() - input.find_first_of(".") - 1;
-  if (input.size() > 1 && input[input.size() - 1] == 'f')
-	--precision;
-
-  print(input, precision);
+void ScalarConverter::convertChar(const std::string &input) {
+  _char = input[0];
+  _int = static_cast<int>(_char);
+  _float = static_cast<float>(_char);
+  _double = static_cast<double>(_char);
 }
 
-void ScalarConverter::print(const std::string &input, int precision) {
-  if (input.size() == 1 && !std::isdigit(input[0]))
-	_double = static_cast<double>(input[0]);
-  else
-	_double = std::atof(input.c_str());
+bool ScalarConverter::isOverflow(const std::string &input) {
+  size_t len = input.size();
+  if (input[0] == '-' || input[0] == '+')
+	--len;
 
-  if (_double < 0 || _double > 127)
+  if (len > 10) {
+	_isImpossible = true;
+	return true;
+  }
+
+  if (len == 10) {
+	if (input[0] == '-') {
+	  if (input.compare("-2147483648") > 0) {
+		_isImpossible = true;
+		return true;
+	  }
+	} else if (input[0] == '+') {
+	  if (input.compare("+2147483647") > 0) {
+		_isImpossible = true;
+		return true;
+	  }
+	} else {
+	  if (input.compare("2147483647") > 0) {
+		_isImpossible = true;
+		return true;
+	  }
+	}
+  }
+  return false;
+}
+
+void ScalarConverter::convertInt(const std::string &input) {
+  if (isOverflow(input)) {
+	_isImpossible = true;
+	return;
+  }
+  _int = std::atoi(input.c_str());
+
+  if (_int < std::numeric_limits<char>::min() ||
+	  std::numeric_limits<char>::max() < _int)
+	_isCharImpossible = true;
+  else
+	_char = static_cast<char>(_int);
+
+  _float = static_cast<float>(_int);
+  _double = static_cast<double>(_int);
+}
+
+void ScalarConverter::convertFloat(const std::string &input) {
+  _float = static_cast<float>(std::atof(input.c_str()));
+
+  if (_float < static_cast<float>(std::numeric_limits<char>::min()) ||
+	  static_cast<float>(std::numeric_limits<char>::max()) < _float)
+	_isCharImpossible = true;
+  else
+	_char = static_cast<char>(_float);
+
+  if (_float < static_cast<float>(std::numeric_limits<int>::min()) ||
+	  static_cast<float>(std::numeric_limits<int>::max()) < _float)
+	_isIntImpossible = true;
+  else
+	_int = static_cast<int>(_float);
+
+  _double = static_cast<double>(_float);
+}
+
+void ScalarConverter::convertDouble(const std::string &input) {
+  _double = std::atof(input.c_str());
+
+  if (_double < std::numeric_limits<char>::min() ||
+	  std::numeric_limits<char>::max() < _double)
+	_isCharImpossible = true;
+  else
+	_char = static_cast<char>(_double);
+
+  if (_double < static_cast<double>(std::numeric_limits<int>::min()) ||
+	  static_cast<double>(std::numeric_limits<int>::max()) < _double)
+	_isIntImpossible = true;
+  else
+	_int = static_cast<int>(_double);
+
+  _float = static_cast<float>(_double);
+}
+
+void ScalarConverter::print(int precision) {
+  if (_isCharImpossible || _isImpossible)
 	std::cout << "char: impossible" << std::endl;
   else {
-	_char = static_cast<char>(_double);
 	if (32 < _char && _char < 127)
 	  std::cout << "char: '" << _char << "'" << std::endl;
 	else
 	  std::cout << "char: Non displayable" << std::endl;
   }
 
-  if (_double < static_cast<double>(std::numeric_limits<int>::min()) ||
-	  _double > static_cast<double>(std::numeric_limits<int>::max()))
+  if (_isIntImpossible || _isImpossible)
 	std::cout << "int: impossible" << std::endl;
-  else {
-	_int = static_cast<int>(_double);
+  else
 	std::cout << "int: " << _int << std::endl;
-  }
 
-  if (_double < -static_cast<double>(std::numeric_limits<float>::max()) ||
-	  _double > static_cast<double>(std::numeric_limits<float>::max()))
+  if (_isImpossible)
 	std::cout << "float: impossible" << std::endl;
   else {
-	_float = static_cast<float>(_double);
 	std::cout << "float: " << std::fixed << std::setprecision(precision)
 			  << _float
 			  << "f" << std::endl;
   }
 
-  if (_double < -std::numeric_limits<double>::max() ||
-	  _double > std::numeric_limits<double>::max())
+  if (_isImpossible)
 	std::cout << "double: impossible" << std::endl;
-  else
+  else {
 	std::cout << "double: " << std::fixed << std::setprecision(precision)
 			  << _double
 			  << std::endl;
+  }
+}
+
+void ScalarConverter::convert(const std::string &input) {
+  if (isPseudoLiteral(input))
+	return;
+
+  TYPE type = getType(input);
+  if (type == CHAR) {
+	convertChar(input);
+  } else {
+	if (!isNumeric(input))
+	  throw InvalidInput();
+	if (type == INT)
+	  convertInt(input);
+	else if (type == FLOAT)
+	  convertFloat(input);
+	else
+	  convertDouble(input);
+  }
+
+  int precision = 1;
+  if (input.find_first_of(".") != input.npos && input[input.size() - 1] != '.')
+	precision = input.size() - input.find_first_of(".") - 1;
+  if (input.size() > 1 && input[input.size() - 1] == 'f'
+	  && input[input.size() - 2] != '.')
+	--precision;
+
+  print(precision);
 }
--- cpp_module/module_06/ex00/scalarConverter.hpp
+++ cpp_module/module_06/ex00/scalarConverter.hpp
@@ -7,6 +7,13 @@
 #include <cstdlib>
 #include <limits>
 
+enum TYPE {
+  CHAR,
+  INT,
+  FLOAT,
+  DOUBLE
+};
+
 class ScalarConverter {
  private:
   static char _char;
@@ -14,14 +21,25 @@ class ScalarConverter {
   static float _float;
   static double _double;
 
+  static bool _isImpossible;
+  static bool _isCharImpossible;
+  static bool _isIntImpossible;
+
   ScalarConverter();
   ScalarConverter(const ScalarConverter &);
   ScalarConverter &operator=(const ScalarConverter &);
   ~ScalarConverter();
 
   static bool isPseudoLiteral(const std::string &input);
+  static TYPE getType(const std::string &input);
   static bool isNumeric(const std::string &input);
-  static void print(const std::string &input, int precision);
+
+  static void convertChar(const std::string &input);
+  static bool isOverflow(const std::string &input);
+  static void convertInt(const std::string &input);
+  static void convertFloat(const std::string &input);
+  static void convertDouble(const std::string &input);
+  static void print(int precision);
 
  public:
   static void convert(const std::string &input);
