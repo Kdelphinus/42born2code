2fc573ce46643303ba87a3012c6bb94cd4c3eb0b
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-09 15:37:54 +0900

    :recycle: 코드 정리
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_06/ex02/main.cpp
+++ cpp_module/module_06/ex02/main.cpp
@@ -3,62 +3,62 @@
 #include "C.hpp"
 
 Base *generate(void) {
-	std::srand(std::time(0));
-	int i = std::rand() % 3;
+  std::srand(std::time(0));
+  int i = std::rand() % 3;
 
-	std::cout << "Create: ";
-	if (i == 0) {
-		std::cout << "Type A" << std::endl;
-		return new A();
-	} else if (i == 1) {
-		std::cout << "Type B" << std::endl;
-		return new B();
-	} else {
-		std::cout << "Type C" << std::endl;
-		return new C();
-	}
+  std::cout << "Create: ";
+  if (i == 0) {
+	std::cout << "Type A" << std::endl;
+	return new A();
+  } else if (i == 1) {
+	std::cout << "Type B" << std::endl;
+	return new B();
+  } else {
+	std::cout << "Type C" << std::endl;
+	return new C();
+  }
 }
 
 void identify(Base *p) {
-	std::cout << "Pointer: ";
-	if (dynamic_cast<A *>(p))
-		std::cout << "Type A" << std::endl;
-	else if (dynamic_cast<B *>(p))
-		std::cout << "Type B" << std::endl;
-	else if (dynamic_cast<C *>(p))
-		std::cout << "Type C" << std::endl;
-	return;
+  std::cout << "Pointer: ";
+  if (dynamic_cast<A *>(p))
+	std::cout << "Type A" << std::endl;
+  else if (dynamic_cast<B *>(p))
+	std::cout << "Type B" << std::endl;
+  else if (dynamic_cast<C *>(p))
+	std::cout << "Type C" << std::endl;
+  return;
 }
 
 void identify(Base &p) {
-	std::cout << "Reference: ";
-	try {
-		A &a = dynamic_cast<A &>(p);
-		(void)a;
-		std::cout << "Type A" << std::endl;
-	}
-	catch (...) {}
-	try {
-		B &b = dynamic_cast<B &>(p);
-		(void)b;
-		std::cout << "Type B" << std::endl;
-	}
-	catch (...) {}
-	try {
-		C &c = dynamic_cast<C &>(p);
-		(void)c;
-		std::cout << "Type C" << std::endl;
-	}
-	catch (...) {}
+  std::cout << "Reference: ";
+  try {
+	A &a = dynamic_cast<A &>(p);
+	(void)a;
+	std::cout << "Type A" << std::endl;
+  }
+  catch (...) {}
+  try {
+	B &b = dynamic_cast<B &>(p);
+	(void)b;
+	std::cout << "Type B" << std::endl;
+  }
+  catch (...) {}
+  try {
+	C &c = dynamic_cast<C &>(p);
+	(void)c;
+	std::cout << "Type C" << std::endl;
+  }
+  catch (...) {}
 }
 
 int main() {
-	Base *p = generate();
-	Base &r = *p;
+  Base *p = generate();
+  Base &r = *p;
 
-	identify(p);
-	identify(r);
+  identify(p);
+  identify(r);
 
-	delete p;
-	return 0;
+  delete p;
+  return 0;
 }
