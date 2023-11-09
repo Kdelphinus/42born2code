3f7ded2802f00f2c0e9ff424e3d563cc0492466a
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-09 15:32:41 +0900

    :package: 사용하는 파일 이름 변경
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_06/ex00/main.cpp
+++ cpp_module/module_06/ex00/main.cpp
@@ -1,15 +1,15 @@
-#include "scalarConverter.hpp"
+#include "ScalarConverter.hpp"
 
 int main(int ac, char **av) {
-	if (ac != 2) {
-		std::cout << "Usage: ./convert [input]" << std::endl;
-		return 1;
-	}
-	try {
-		ScalarConverter::convert(av[1]);
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
-	return 0;
+  if (ac != 2) {
+	std::cout << "Usage: ./convert [input]" << std::endl;
+	return 1;
+  }
+  try {
+	ScalarConverter::convert(av[1]);
+  } catch (std::exception &e) {
+	std::cerr << e.what() << std::endl;
+	return 1;
+  }
+  return 0;
 }
