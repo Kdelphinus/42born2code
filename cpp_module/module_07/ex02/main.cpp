8b50ebf892504f3dfb9ef1bb02e618bf36c758f5
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-09 14:55:05 +0900

    :white_check_mark: 예외처리 동작 변경

--- cpp_module/module_07/ex02/main.cpp
+++ cpp_module/module_07/ex02/main.cpp
@@ -1,59 +1,48 @@
 #include <iostream>
 #include <Array.hpp>
 
-#define MAX_VAL 750
-int main(int, char**)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
+int main(int, char **) {
+  int maxVal = 750;
+  Array<int> numbers(maxVal);
+  int *mirror = new int[maxVal];
+  srand(time(NULL));
+  for (int i = 0; i < maxVal; i++) {
+	const int value = rand();
+	numbers[i] = value;
+	mirror[i] = value;
+  }
 
-		tmp[0] = 21;
-		test[0] = 42;
-		std::cout << "numbers[0]: " << numbers[0] << std::endl;
-		std::cout << "tmp[0]: " << tmp[0] << std::endl;
-		std::cout << "test[0]: " << test[0] << std::endl;
-    }
+  {
+	Array<int> tmp = numbers;
+	Array<int> test(tmp);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	tmp[0] = 21;
+	test[0] = 42;
+	std::cout << "numbers[0]: " << numbers[0] << std::endl;
+	std::cout << "tmp[0]: " << tmp[0] << std::endl;
+	std::cout << "test[0]: " << test[0] << std::endl;
+  }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;
-    return 0;
+  try {
+	for (int i = 0; i < maxVal; i++) {
+	  if (mirror[i] != numbers[i])
+		throw std::exception();
+	}
+  } catch (const std::exception &e) {
+	std::cout << "Error: didn't save the same value!!" << std::endl;
+  }
+  try {
+	numbers[-2] = 0;
+  }
+  catch (const std::exception &e) {
+	std::cout << "Error: minus index" << std::endl;
+  }
+  try {
+	numbers[maxVal] = 0;
+  }
+  catch (const std::exception &e) {
+	std::cout << "Error: max index" << std::endl;
+  }
+  delete[] mirror;
+  return 0;
 }
