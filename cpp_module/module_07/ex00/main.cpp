2374c1cbb6a4701848614b8ee9d3a1f17a646bbb
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-10 22:08:59 +0900

    :white_check_mark: 출력문 추가

--- cpp_module/module_07/ex00/main.cpp
+++ cpp_module/module_07/ex00/main.cpp
@@ -4,8 +4,9 @@ int main() {
   int a = 2;
   int b = 3;
 
+  std::cout << "Before: " << "a = " << a << ", b = " << b << std::endl;
   swap(a, b);
-  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "After: " << "a = " << a << ", b = " << b << std::endl;
   std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
   std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
