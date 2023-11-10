42c0e166b0653c893b142338c336bd2ba3f5d252
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-10 16:30:13 +0900

    :art: 생성자, 소멸자 private으로 이동
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- cpp_module/module_06/ex01/Serializer.hpp
+++ cpp_module/module_06/ex01/Serializer.hpp
@@ -11,11 +11,11 @@ struct Data {
 
 class Serializer {
  private:
+  Serializer();
   Serializer(const Serializer &se);
   Serializer &operator=(const Serializer &se);
- public:
-  Serializer();
   ~Serializer();
+ public:
 
   static uintptr_t serialize(Data *ptr);
   static Data *deserialize(uintptr_t raw);
--- cpp_module/module_06/ex01/main.cpp
+++ cpp_module/module_06/ex01/main.cpp
@@ -1,13 +1,12 @@
 #include "Serializer.hpp"
 
 int main() {
-  static Serializer se;
   Data *ptr = new Data;
   ptr->n = 42;
   ptr->s1 = "Hello";
   ptr->s2 = "World";
-  uintptr_t raw = se.serialize(ptr);
-  Data *ptr2 = se.deserialize(raw);
+  uintptr_t raw = Serializer::serialize(ptr);
+  Data *ptr2 = Serializer::deserialize(raw);
   std::cout << ptr2->n << std::endl;
   std::cout << ptr2->s1 << std::endl;
   std::cout << ptr2->s2 << std::endl;
