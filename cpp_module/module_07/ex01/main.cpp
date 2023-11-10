606643b60153bd73b8c58c05423794b901b6f1a1
Myoung Jun Ko <kmj951015@gmail.com>
2023-11-10 22:07:07 +0900

    :recycle: 코드 재정렬

--- cpp_module/module_07/ex01/main.cpp
+++ cpp_module/module_07/ex01/main.cpp
@@ -6,16 +6,16 @@ void print(T &x) {
 }
 
 int main() {
-  char tab_char[] = {'a', 'b', 'c', 'd', 'e'};
+  char tabChar[] = {'a', 'b', 'c', 'd', 'e'};
   int tab[] = {0, 1, 2, 3, 4};
-  double tab_double[] = {0.0, 1.1, 2.2, 3.3, 4.4};
+  double tabDouble[] = {0.0, 1.1, 2.2, 3.3, 4.4};
 
-  std::cout << "iter(tab_char, 5, print<const char>): ";
-  iter(tab_char, 5, print<const char>);
+  std::cout << "iter(tabChar, 5, print<const char>): ";
+  iter(tabChar, 5, print<const char>);
   std::cout << std::endl;
 
-  std::cout << "iter(tab_char, 5, print<char>): ";
-  iter(tab_char, 5, print<char>);
+  std::cout << "iter(tabChar, 5, print<char>): ";
+  iter(tabChar, 5, print<char>);
   std::cout << std::endl;
 
   std::cout << "iter(tab, 5, print<const int>): ";
@@ -26,12 +26,12 @@ int main() {
   iter(tab, 5, print<int>);
   std::cout << std::endl;
 
-  std::cout << "iter(tab_double, 5, print<const double>): ";
-  iter(tab_double, 5, print<const double>);
+  std::cout << "iter(tabDouble, 5, print<const double>): ";
+  iter(tabDouble, 5, print<const double>);
   std::cout << std::endl;
 
-  std::cout << "iter(tab_double, 5, print<double>): ";
-  iter(tab_double, 5, print<double>);
+  std::cout << "iter(tabDouble, 5, print<double>): ";
+  iter(tabDouble, 5, print<double>);
   std::cout << std::endl;
   return 0;
 }
