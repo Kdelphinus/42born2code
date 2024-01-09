1b240d9dfca329bed902a4a5d59d8ccbf996a893
Myoung Jun Ko <kmj951015@gmail.com>
2024-01-09 16:53:43 +0900

    :art: inven에 Amateria * 를 가지도록 수정

--- cpp_module/module_04/ex03/Character.cpp
+++ cpp_module/module_04/ex03/Character.cpp
@@ -1,82 +1,89 @@
 #include "Character.hpp"
 
 Character::Character() : name(), invenIdx() {
-    for (int i = 0; i < 4; i++)
-        this->invenIdx[i] = false;
+  for (int i = 0; i < 4; i++)
+	this->invenIdx[i] = false;
 }
 
 Character::Character(const std::string &name) : name(name), invenIdx() {
-    for (int i = 0; i < 4; i++)
-        this->invenIdx[i] = false;
+  for (int i = 0; i < 4; i++)
+	this->invenIdx[i] = false;
 }
 
-Character::Character(const Character &character) : name(character.getName()), invenIdx() {
-    for (int i = 0; i < 4; i++) {
-        if (character.getInvenIdx(i)) {
-            this->inven[i] = character.getInven(i);
-            this->invenIdx[i] = true;
-        }
-    }
+Character::Character(const Character &character) : name(character.getName()),
+												   invenIdx() {
+  for (int i = 0; i < 4; i++) {
+	if (character.getInvenIdx(i)) {
+	  this->inven[i] = character.getInven(i);
+	  this->invenIdx[i] = true;
+	}
+  }
 }
 
 Character &Character::operator=(const Character &character) {
-    if (this != &character) {
-        this->name = character.getName();
-        for (int i = 0; i < 4; i++) {
-            if (character.getInvenIdx(i)) {
-                this->inven[i] = character.getInven(i);
-                this->invenIdx[i] = true;
-            }
-        }
-    }
-    return *this;
+  if (this != &character) {
+	this->name = character.getName();
+	for (int i = 0; i < 4; i++) {
+	  if (character.getInvenIdx(i)) {
+		this->inven[i] = character.getInven(i);
+		this->invenIdx[i] = true;
+	  }
+	}
+  }
+  return *this;
 }
 
 Character::~Character() {}
 
 const std::string &Character::getName() const {
-    return this->name;
+  return this->name;
 }
 
 bool Character::getInvenIdx(int idx) const {
-    return this->invenIdx[idx];
+  return this->invenIdx[idx];
 }
 
-MateriaSource Character::getInven(int idx) const {
-    if (idx < 0 || idx >= 4 || !this->invenIdx[idx]) {
-        std::cout << "Invalid index" << std::endl;
-        return this->inven[0];
-    }
-    return this->inven[idx];
+AMateria *Character::getInven(int idx) const {
+  if (idx < 0 || idx >= 4 || !this->invenIdx[idx]) {
+	std::cout << "Invalid index" << std::endl;
+	return this->inven[0];
+  }
+  return this->inven[idx];
 }
 
 void Character::equip(AMateria *m) {
-    if (!m) {
-        std::cout << "Invalid materia" << std::endl;
-        return;
-    }
-    for (int i = 0; i < 4; i++) {
-        if (!this->invenIdx[i]) {
-            this->inven[i].setSource(m);
-            this->invenIdx[i] = true;
-            return;
-        }
-    }
-    std::cout << "Inventory full" << std::endl;
+  if (!m) {
+	std::cout << "Invalid materia" << std::endl;
+	return;
+  }
+  for (int i = 0; i < 4; i++) {
+	if (this->inven[i] == m) {
+	  std::cout << "Materia already equipped" << std::endl;
+	  return;
+	}
+  }
+  for (int i = 0; i < 4; i++) {
+	if (!this->invenIdx[i]) {
+	  this->inven[i] = m;
+	  this->invenIdx[i] = true;
+	  return;
+	}
+  }
+  std::cout << "Inventory full" << std::endl;
 }
 
 void Character::unequip(int idx) {
-    if (idx < 0 || idx >= 4 || !this->invenIdx[idx]) {
-        std::cout << "Invalid index" << std::endl;
-        return;
-    }
-    this->invenIdx[idx] = false;
+  if (idx < 0 || idx >= 4 || !this->invenIdx[idx]) {
+	std::cout << "Invalid index" << std::endl;
+	return;
+  }
+  this->invenIdx[idx] = false;
 }
 
 void Character::use(int idx, ICharacter &target) {
-    if (idx < 0 || idx >= 4 || !this->invenIdx[idx] || !this->inven[idx].getSource()) {
-        std::cout << "Invalid index" << std::endl;
-        return;
-    }
-    this->inven[idx].getSource()->use(target);
+  if (idx < 0 || idx >= 4 || !this->invenIdx[idx] || !this->inven[idx]) {
+	std::cout << "Invalid index" << std::endl;
+	return;
+  }
+  this->inven[idx]->use(target);
 }
--- cpp_module/module_04/ex03/Character.hpp
+++ cpp_module/module_04/ex03/Character.hpp
@@ -2,26 +2,26 @@
 #define CHARACTER_HPP
 
 #include "ICharacter.hpp"
-#include "MateriaSource.hpp"
+#include "AMateria.hpp"
 
 class Character : public ICharacter {
-    private:
-        std::string name;
-        bool invenIdx[4];
-        MateriaSource inven[4];
-    public:
-        Character();
-        explicit Character(const std::string &name);
-        Character(const Character &character);
-        Character &operator=(const Character &character);
-        ~Character();
+ private:
+  std::string name;
+  bool invenIdx[4];
+  AMateria *inven[4];
+ public:
+  Character();
+  explicit Character(const std::string &name);
+  Character(const Character &character);
+  Character &operator=(const Character &character);
+  ~Character();
 
-        std::string const &getName() const;
-        bool getInvenIdx(int idx) const;
-        MateriaSource getInven(int idx) const;
-        void equip(AMateria *m);
-        void unequip(int idx);
-        void use(int idx, ICharacter &target);
+  std::string const &getName() const;
+  bool getInvenIdx(int idx) const;
+  AMateria *getInven(int idx) const;
+  void equip(AMateria *m);
+  void unequip(int idx);
+  void use(int idx, ICharacter &target);
 };
 
 #endif
