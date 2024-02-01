1007d3b3217b9b2f85973535384adeab9645196f
Myoung Jun Ko <kmj951015@gmail.com>
2024-02-01 20:45:33 +0900

    :bug: WP_NAME과 DOMAIN_NAME 교체

--- inception/srcs/requirements/wordpress/tools/configuration.sh
+++ inception/srcs/requirements/wordpress/tools/configuration.sh
@@ -6,7 +6,7 @@ if [ ! -e "/var/www/html/wp-config.php" ]; then
     cd /var/www/html
     wp core download --allow-root
     wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
-    wp core install --url=$WP_NAME --title=$DOMAIN_NAME --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+    wp core install --url=$DOMAIN_NAME --title=$WP_NAME --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
     wp user create ${WP_USER} $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 fi
 
